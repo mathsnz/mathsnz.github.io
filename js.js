b47ccea3079da5ea43f72c7ac1bf8a16dd81b4ee
mathsnz <jwills@mathsnz.com>
2015-08-19 17:06:59 +1200

    update

--- js.js
+++ js.js
@@ -2,9 +2,11 @@ function init() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             minsize = 30,
-			leftright = 10;
+			leftright = 40;
             header = document.getElementById("header"),
-			size = 100 - distanceY;
+			size = 100 - distanceY,
+			imgheight = 70 - distanceY,
+			maxheight = 70 - leftright;
         if (minsize > size) {
             $("#header").css( "height", minsize+"px" );
         } else {
@@ -13,9 +15,11 @@ function init() {
 		if (distanceY > leftright) {
             $("#header img").css( "float", "left" );
             $("#nav").css( "text-align", "right" );
+            $("#header img").css( "max-height", maxheight+"px" );
 		} else {
             $("#header img").css( "float", "initial" );
             $("#nav").css( "text-align", "center" );
+            $("#header img").css( "max-height", imgheight+"px" );
 		}
     });
 }
